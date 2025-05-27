95dc4784c671af9806e12ebc8a7ef5304bde9541
JCKawin <jckawin2007@gmail.com>
2025-05-27 23:24:28 +0530

    adjustments for testing

--- mainfile.py
+++ mainfile.py
@@ -5,6 +5,7 @@ class main:
     def __init__(self):
         self.screen = pygame.display.set_mode(Set.SCREEN_SIZE)
         self.clock = pygame.time.Clock()
+        self.running = True
 
 
     def gameloop(self):
