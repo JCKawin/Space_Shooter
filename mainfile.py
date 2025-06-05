cd04e61cec6e9982b3ceb69f49984c3c1df0633b
JCKawin <jckawin2007@gmail.com>
2025-06-05 23:52:08 +0530

    ship added

--- mainfile.py
+++ mainfile.py
@@ -6,27 +6,30 @@ class main:
     def __init__(self):
         self.screen = pygame.display.set_mode(Set.SCREEN_SIZE)
         self.clock = pygame.time.Clock()
+        
         self.running = True
+        
         self.ship = ship.ship()
         self.dt = 0
         
     def gameloop(self):
         while self.running:
-            self._update_events()
-            self.dt = self.clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            self.dt = self.clock.tick()
             self.screen.fill(Set.BG_COLOR)
-            self.ship.update(self.dt)
             self.screen.blit(self.ship.image , self.ship.rect)
             pygame.display.flip()
+            self.ship.update(self.dt)        
 
-    def _update_events(self):
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                self.running = False
-            if e.type == pygame.KEYDOWN:
-                self.ship.ismoving = True
-            if e.type == pygame.KEYUP:
-                self.ship.ismoving = False
+    def _draw_fps(self):
+        fps = self.font.render(self.clock.get_fps(),True ,(0,0,0))
+        fps_rect = fps.get_frect()
+        fps_rect.topleft = (0,0)
+
+
+        
             
     
 
--- ship.py
+++ ship.py
@@ -1,25 +1,21 @@
+
 import pygame
 import settings as Set
 
 class ship():
-    def __init__(self):
-       
+    def __init__(self ):
+        
         self.image = pygame.image.load("images\\ship.jpg")
         self.image = pygame.transform.scale(self.image , (128 , 128))
-        self.rect = self.image.get_frect()
-        self.direction = pygame.Vector2()
+        self.rect = self.image.get_rect(center = (Set.SCREEN_SIZE[0]/2 , Set.SCREEN_SIZE[1]/2))
+        self.direction = pygame.Vector2(0,0)
         self.ismoving = False
 
     def update(self , dt):
-        if self.ismoving:
-            self.move(dt)
+        keypressed = pygame.key.get_pressed()
+        self.direction.x = (int(keypressed[pygame.K_d]) - int(keypressed[pygame.K_a])) * Set.MOVEMENT_SPEED * dt
+        self.direction.y = (int(keypressed[pygame.K_s]) - int(keypressed[pygame.K_w])) * Set.MOVEMENT_SPEED * dt
 
-    
-    def move(self ,dt):
-        key = pygame.key.get_pressed()
-        self.direction.x = (int(key[pygame.K_d]) - int(key[pygame.K_a])) * Set.MOVEMENT_SPEED / dt
-        self.direction.y = (int(key[pygame.K_s]) - int(key[pygame.K_w])) * Set.MOVEMENT_SPEED / dt
-        self.direction = self.direction.normalize() if self.direction else self.direction
         self.rect.center += self.direction
         
 
