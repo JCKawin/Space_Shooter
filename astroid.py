facd2487a2867064afcc313017a87ef8d8217905
JCKawin <jckawin2007@gmail.com>
2025-06-10 23:34:26 +0530

    astroid added

--- astroid.py
+++ astroid.py
@@ -1,11 +1,16 @@
+import settings as Set
 import pygame
 
 
 class Rock(pygame.sprite.Sprite):
-    def __init__(self, group) -> None:
+    def __init__(self, group , location) -> None:
         super().__init__(group)
         self.image = pygame.image.load("images\\proto#astroid.png").convert_alpha()
-        self.rect = self.image.get_frect()
+        self.image = pygame.transform.scale(self.image , (64,64))
+        self.rect = self.image.get_frect(center = location)
+
+    def update(self , dt) -> None:
+        self.rect.centery += Set.MOVEMENT_SPEED * dt
 
 
     
--- mainfile.py
+++ mainfile.py
@@ -1,6 +1,8 @@
+import random
 import pygame
 import settings as Set
 import ship
+import astroid
 
 class main:
     def __init__(self):
@@ -10,21 +12,29 @@ class main:
         self.screen = pygame.display.set_mode(Set.SCREEN_SIZE , pygame.RESIZABLE)
         self.clock = pygame.time.Clock()
         self.Background = pygame.image.load("images\\proto#background.bmp")
-        
+        self.Rock = pygame.sprite.Group()        
         self.running = True
         
         self.ship = ship.ship(self)
         self.dt = 0
         
     def gameloop(self):
+        put_astroid = pygame.event.custom_type()
+        pygame.time.set_timer(put_astroid , 1000)
+        
         while self.running:
+            rock_point = random.randint(0 , Set.SCREEN_SIZE[0]) , random.randint(0 , 20) 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                if event.type == put_astroid:
+                    astroid.Rock(self.Rock , rock_point)
             self.dt = self.clock.tick()
             self.screen.blit(self.Background , (0,0))
             self.screen.blit(self.ship.image , self.ship.rect)
+            self.Rock.draw(self.screen)
             pygame.display.flip()
+            self.Rock.update(self.dt)       
             self.ship.update(self.dt)        
 
     
