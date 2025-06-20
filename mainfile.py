46517dc2cf7c2eabc59b228928e43e8356f4aa0a
JD-2572007 <danieljosephjd2007@gmail.com>
2025-06-20 20:25:52 +0530

    corrections

--- mainfile.py
+++ mainfile.py
@@ -32,7 +32,17 @@ class main:
         self.bul_img = pygame.image.load("images\\proto#bullet.png").convert_alpha()
         self.rock_img = pygame.image.load("images\\proto#astroid.png").convert_alpha()
         self.rock_img = pygame.transform.scale(self.rock_img, (64, 64))
-        self.bgm = pygame.mixer.music.load("audio\\Project_Space Shooter_Final.mp3")
+        self.bgm = pygame.mixer.music.load("audio\\Project_Space Shooter_Final_Loop.mp3")
+        pygame.mixer.music.set_volume(0.5)
+        self.shoot_eff = pygame.mixer.Sound("audio\\Space Shooter_Fire.mp3")
+        self.rock_exp_eff = pygame.mixer.Sound("audio\\Space Shooter_Explosion.mp3")
+        self.rock_impact = pygame.mixer.Sound("audio\\Space Shooter_Asteroid Impact.mp3")
+        self.ship_heal_eff = pygame.mixer.Sound("audio\\Space Shooter_Healing.mp3")
+        self.f_uwl = pygame.font.Font("fonts\\VT323-Regular.ttf" , 30)
+        self.bul_img = pygame.image.load("images\\proto#bullet.png").convert_alpha()
+        self.rock_img = pygame.image.load("images\\proto#astroid.png").convert_alpha()
+        self.rock_img = pygame.transform.scale(self.rock_img , (64,64))
+        self.bgm = pygame.mixer.music.load("audio\\Project_Space Shooter_Final_Loop.mp3")
         pygame.mixer.music.set_volume(0.5)
         self.shoot_eff=pygame.mixer.Sound("audio\\Space Shooter_Fire.mp3")
         self.rock_exp_eff=pygame.mixer.Sound("audio\\Space Shooter_Explosion.mp3")
@@ -46,10 +56,11 @@ class main:
         pygame.mixer.music.play(loops=-1)
 
         while self.running:
-            rock_point = random.randint(0, Set.SCREEN_SIZE[0]), random.randint(0, 20)
+            rock_point = random.randint(
+                0, Set.SCREEN_SIZE[0]), random.randint(0, 20)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                  self.running = False
+                    self.running = False
                 if event.type == put_astroid:
                     astroid.Rock(self.rock, rock_point, self.rock_img)
                 if event.type == pygame.KEYDOWN:
@@ -59,7 +70,7 @@ class main:
 
                     if event.key == pygame.K_l:
                         self._heal()
-
+            pygame.mixer.Sound.play(self.ship_heal_eff)
             self.screen.fill(Set.BG_COLOR)
             self.screen.blit(self.background, (0, 0))
             self.screen.blit(self.ship.image, self.ship.rect)
@@ -102,15 +113,11 @@ class main:
             pygame.mixer.Sound.play(self.ship_heal_eff)
 
     def _UI(self):
-        healthbar = pygame.rect.Rect(
-            30, Set.SCREEN_SIZE[1] - 50, self.ship.Hp * 4, 20)
+        healthbar = pygame.rect.Rect(30, Set.SCREEN_SIZE[1] - 50, self.ship.Hp * 4, 20)
         pygame.draw.rect(self.screen, self.health_bar_colour, healthbar)
-        self.printf(self.screen, f"health {self.ship.Hp}",
-                    (34, Set.SCREEN_SIZE[1] - 55), 'black', self.f_uwl)
-        self.printf(
-            self.screen, f"FPS: {round(self.clock.get_fps(), 0)}", (30, 30), 'white', self.f_pkl)
-        self.printf(self.screen, f"Score : {self.score}",
-                    (Set.SCREEN_SIZE[0] - 120, 20), 'white', self.f_pkl)
+        self.printf(self.screen, f"health {self.ship.Hp}", (34, Set.SCREEN_SIZE[1] - 55), 'black', self.f_uwl)
+        self.printf(self.screen, f"FPS: {round(self.clock.get_fps(), 0)}", (30, 30), 'white', self.f_pkl)
+        self.printf(self.screen, f"Score : {self.score}",(Set.SCREEN_SIZE[0] - 120, 20), 'white', self.f_pkl)
 
     def gameover(self):
         pass
